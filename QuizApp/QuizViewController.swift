0924dc50eaa6c453f43dd456579622fee6fbb68c
maropook <ii440092@icloud.com>
2021-08-16 16:29:44 +0900

    fix

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -28,6 +28,7 @@ class QuizViewController: UIViewController {
         
         print("選択したのはレベル\(selectLevel)")
         csvArray = loadCSV(fileName: "quiz\(selectLevel)")
+        csvArray.shuffle()
         print(csvArray)
         
         quizArray = csvArray[quizCount].components(separatedBy: ",")
--- QuizApp/ScoreViewController.swift
+++ QuizApp/ScoreViewController.swift
@@ -18,19 +18,13 @@ class ScoreViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func shareButtonAction(_ sender: Any) {
+        let activityItems = ["\(correct)問正解しました.","#クイズアプリ"]
+                let activityVC = UIActivityViewController (activityItems: activityItems, applicationActivities: nil)
+                self.present(activityVC, animated: true)
+    }
     @IBAction func toTopButtonAction(_ sender: Any) {
         
         self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
